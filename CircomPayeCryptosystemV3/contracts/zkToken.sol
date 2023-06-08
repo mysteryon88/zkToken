83e661169f47cb4d2721f264c4cdc0db3126c293
Sergey <68071039+mysteryon88@users.noreply.github.com>
2023-06-09 01:31:25 +0300

    gas optimization tests

---
+++ CircomPayeCryptosystemV3/contracts/zkToken.sol
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./IVerifier.sol";
+
+/// @title zkToken
+
+contract zkToken {
+    string public name = "zkToken";
+    string public symbol = "ZKT";
+    uint256 public decimals = 0;
+
+    IVerifier private transferVerifierAddr;
+    IVerifier private registrationVerifierAddr;
+    IVerifier private mintVerifierAddr;
+
+    struct Key {
+        uint256 g;
+        uint256 n;
+        uint256 powN2;
+    }
+
+    struct User {
+        uint256 encryptedBalance;
+        Key key;
+    }
+
+    mapping(address => User) private users;
+
+    event Registration(address indexed _who);
+    event Mint(address indexed _to);
+    event Transfer(address indexed _to);
+
+    error WrongProof(string _error);
+
+    /* name, symbol, decimals */
+    constructor(
+        address _transferVerifieqAddr,
+        address _registrationVerifierAddr,
+        address _mintVerifierAddr
+    ) {
+        transferVerifierAddr = IVerifier(_transferVerifieqAddr);
+        registrationVerifierAddr = IVerifier(_registrationVerifierAddr);
+        mintVerifierAddr = IVerifier(_mintVerifierAddr);
+    }
+
+    /// @notice Getting a user's balance
+    /// @param _to user address
+    /// @return encryptedBalance encrypted balance
+    function balanceOf(address _to) external view returns (uint256) {
+        return users[_to].encryptedBalance;
+    }
+
+    function getPubKey(address _to) external view returns (Key memory) {
+        return users[_to].key;
+    }
+
+    /* onlyFee */
+    function registration(
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint /*4*/[] memory input
+    ) external payable /* onlyFee */ {
+        require(input[0] >= 0, "wrong balance value");
+        // input = balance, key.g, r, key.n
+        require(input[1] >= 0 && input[3] >= 0, "invalid key value");
+        require(users[msg.sender].encryptedBalance == 0, "you are registered");
+
+        bool registrationProofIsCorrect = registrationVerifierAddr.verifyProof(
+            a,
+            b,
+            c,
+            input
+        );
+
+        User storage user = users[msg.sender];
+
+        if (registrationProofIsCorrect) {
+            user.encryptedBalance = input[0];
+            user.key.g = input[1];
+            user.key.n = input[3];
+            user.key.powN2 = input[3] * input[3];
+            emit Registration(msg.sender);
+        } else revert WrongProof("Wrong proof");
+    }
+
+    function mint(
+        address _to,
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint /*4*/[] memory input
+    ) external onlyRegistered(_to) zeroAddress(_to) {
+
+        bool mintProofIsCorrect = mintVerifierAddr.verifyProof(a, b, c, input);
+
+        User storage user = users[_to];
+
+        if (mintProofIsCorrect) {
+            unchecked {
+                user.encryptedBalance =
+                    (user.encryptedBalance * input[0]) %
+                    user.key.powN2;
+            }
+            emit Mint(_to);
+        } else revert WrongProof("Wrong proof");
+    }
+
+    function transfer(
+        address _to,
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint /*9*/[] memory input
+    ) external payable /* onlyFee */ onlyRegistered(_to) zeroAddress(_to) {
+        require(msg.sender != _to, "You cannot send tokens to yourself");
+
+        bool transferProofIsCorrect = transferVerifierAddr.verifyProof(
+            a,
+            b,
+            c,
+            input
+        );
+
+        User storage user = users[_to];
+
+        if (transferProofIsCorrect) {
+            unchecked {
+                user.encryptedBalance =
+                    (user.encryptedBalance * input[1]) %
+                    user.key.powN2;
+            }
+
+            users[msg.sender].encryptedBalance = input[2];
+            emit Transfer(_to);
+        } else revert WrongProof("Wrong proof");
+    }
+
+    modifier onlyFee() {
+        require(msg.value >= 0.001 ether, "Not enough fee!");
+        _;
+    }
+
+    modifier onlyRegistered(address _to) {
+        require(users[_to].encryptedBalance != 0, "user not registered");
+        _;
+    }
+
+    modifier zeroAddress(address _to) {
+        require(_to != address(0), "Zero address");
+        _;
+    }
+}
---
+++ CircomPayeCryptosystemV3/test/Tests.js
@@ -0,0 +1,370 @@
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
+
+const registrationProofA = require('./RegistrationProof/proofA.json')
+const registrationPublicA = require('./RegistrationProof/publicA.json')
+const registrationInputA = require('./RegistrationProof/inputA.json')
+
+const registrationProofB = require('./RegistrationProof/proofB.json')
+const registrationPublicB = require('./RegistrationProof/publicB.json')
+const registrationInputB = require('./RegistrationProof/inputB.json')
+
+const mintProof = require('./MintProof/proof.json')
+const mintPublic = require('./MintProof/public.json')
+const mintInput = require('./MintProof/input.json')
+
+const transferProofA = require('./TransferProof/proofA.json')
+const transferPublicA = require('./TransferProof/publicA.json')
+const transferInputA = require('./TransferProof/inputA.json')
+
+const transferProofB = require('./TransferProof/proofB.json')
+const transferPublicB = require('./TransferProof/publicB.json')
+const transferInputB = require('./TransferProof/inputB.json')
+
+describe('zkToken', function () {
+  let zkToken,
+    registrationVerifier,
+    transferVerifier,
+    mintVerifier,
+    clientA,
+    clientB,
+    clientC
+
+  const fee = ethers.utils.parseUnits('0.001', 'ether')
+
+  before(async function () {
+    ;[clientA, clientB, clientC] = await ethers.getSigners()
+
+    const RegistrationVerifier = await hre.ethers.getContractFactory(
+      'RegistrationVerifier'
+    )
+    registrationVerifier = await RegistrationVerifier.deploy()
+    await registrationVerifier.deployed()
+
+    const TransferVerifier = await hre.ethers.getContractFactory(
+      'TransferVerifier'
+    )
+    transferVerifier = await TransferVerifier.deploy()
+    await transferVerifier.deployed()
+
+    const MintVerifier = await hre.ethers.getContractFactory('MintVerifier')
+    mintVerifier = await MintVerifier.deploy()
+    await mintVerifier.deployed()
+
+    const ZKToken = await hre.ethers.getContractFactory('zkToken')
+    zkToken = await ZKToken.deploy(
+      transferVerifier.address,
+      registrationVerifier.address,
+      mintVerifier.address
+    )
+    await zkToken.deployed()
+  })
+
+  it('name', async function () {
+    expect(await zkToken.name()).to.eq('zkToken')
+  })
+
+  it('symbol', async function () {
+    expect(await zkToken.symbol()).to.eq('ZKT')
+  })
+
+  it('verifyRegistrationProof', async function () {
+    await registrationVerifier.verifyProof(
+      [registrationProofA.pi_a[0], registrationProofA.pi_a[1]],
+      [
+        [registrationProofA.pi_b[0][1], registrationProofA.pi_b[0][0]],
+        [registrationProofA.pi_b[1][1], registrationProofA.pi_b[1][0]],
+      ],
+      [registrationProofA.pi_c[0], registrationProofA.pi_c[1]],
+      registrationPublicA
+    )
+  })
+
+  it('verifyMintProof', async function () {
+    await mintVerifier.verifyProof(
+      [mintProof.pi_a[0], mintProof.pi_a[1]],
+      [
+        [mintProof.pi_b[0][1], mintProof.pi_b[0][0]],
+        [mintProof.pi_b[1][1], mintProof.pi_b[1][0]],
+      ],
+      [mintProof.pi_c[0], mintProof.pi_c[1]],
+      mintPublic
+    )
+  })
+
+  it('verifyTransferProof', async function () {
+    await transferVerifier.verifyProof(
+      [transferProofA.pi_a[0], transferProofA.pi_a[1]],
+      [
+        [transferProofA.pi_b[0][1], transferProofA.pi_b[0][0]],
+        [transferProofA.pi_b[1][1], transferProofA.pi_b[1][0]],
+      ],
+      [transferProofA.pi_c[0], transferProofA.pi_c[1]],
+      transferPublicA
+    )
+  })
+
+  it('registration A', async function () {
+    const tx = await zkToken.connect(clientA).registration(
+      [registrationProofA.pi_a[0], registrationProofA.pi_a[1]],
+      [
+        [registrationProofA.pi_b[0][1], registrationProofA.pi_b[0][0]],
+        [registrationProofA.pi_b[1][1], registrationProofA.pi_b[1][0]],
+      ],
+      [registrationProofA.pi_c[0], registrationProofA.pi_c[1]],
+      registrationPublicA
+    )
+
+    const receipt = await tx.wait()
+
+    console.log(
+      'Gas used by registration: ',
+      '\x1b[33m',
+      receipt.gasUsed.toString(),
+      '\x1b[0m'
+    )
+
+    expect(await zkToken.balanceOf(clientA.address)).to.eq(
+      registrationInputA.encryptedBalance
+    )
+
+    console.log(
+      'Client A balance after registration',
+      await zkToken.balanceOf(clientA.address)
+    )
+  })
+
+  it('registration B', async function () {
+    const tx = await zkToken.connect(clientB).registration(
+      [registrationProofB.pi_a[0], registrationProofB.pi_a[1]],
+      [
+        [registrationProofB.pi_b[0][1], registrationProofB.pi_b[0][0]],
+        [registrationProofB.pi_b[1][1], registrationProofB.pi_b[1][0]],
+      ],
+      [registrationProofB.pi_c[0], registrationProofB.pi_c[1]],
+      registrationPublicB
+    )
+
+    const receipt = await tx.wait()
+
+    console.log(
+      'Gas used by registration: ',
+      '\x1b[33m',
+      receipt.gasUsed.toString(),
+      '\x1b[0m'
+    )
+
+    expect(await zkToken.balanceOf(clientB.address)).to.eq(
+      registrationInputB.encryptedBalance
+    )
+
+    console.log(
+      'Client B balance after registration',
+      await zkToken.balanceOf(clientB.address)
+    )
+  })
+
+  it('mint A', async function () {
+    const tx = await zkToken.mint(
+      clientA.address,
+      [mintProof.pi_a[0], mintProof.pi_a[1]],
+      [
+        [mintProof.pi_b[0][1], mintProof.pi_b[0][0]],
+        [mintProof.pi_b[1][1], mintProof.pi_b[1][0]],
+      ],
+      [mintProof.pi_c[0], mintProof.pi_c[1]],
+      mintPublic
+    )
+
+    expect(
+      decryption(
+        await zkToken.balanceOf(clientA.address),
+        46783589n,
+        11692464n,
+        39229921n
+      )
+    ).to.eq(mintInput.value)
+
+    const receipt = await tx.wait()
+
+    console.log(
+      'Gas used by mint: ',
+      '\x1b[33m',
+      receipt.gasUsed.toString(),
+      '\x1b[0m'
+    )
+
+    console.log(
+      'Client A balance after mint',
+      await zkToken.balanceOf(clientA.address)
+    )
+  })
+
+  it('Revert self-transfer', async function () {
+    await expect(
+      zkToken.connect(clientB).transfer(
+        clientB.address,
+        [transferProofA.pi_a[0], transferProofA.pi_a[1]],
+        [
+          [transferProofA.pi_b[0][1], transferProofA.pi_b[0][0]],
+          [transferProofA.pi_b[1][1], transferProofA.pi_b[1][0]],
+        ],
+        [transferProofA.pi_c[0], transferProofA.pi_c[1]],
+        transferPublicA
+      )
+    ).to.be.revertedWith('you cannot send tokens to yourself')
+  })
+
+  it('Transfer A to B', async function () {
+    const tx = await zkToken.connect(clientA).transfer(
+      clientB.address,
+      [transferProofA.pi_a[0], transferProofA.pi_a[1]],
+      [
+        [transferProofA.pi_b[0][1], transferProofA.pi_b[0][0]],
+        [transferProofA.pi_b[1][1], transferProofA.pi_b[1][0]],
+      ],
+      [transferProofA.pi_c[0], transferProofA.pi_c[1]],
+      transferPublicA
+    )
+
+    const receipt = await tx.wait()
+
+    console.log(
+      'Gas used by transfer: ',
+      '\x1b[33m',
+      receipt.gasUsed.toString(),
+      '\x1b[0m'
+    )
+
+    expect(await zkToken.balanceOf(clientA.address)).to.eq(
+      transferInputA.newEncryptedBalance
+    )
+
+    expect(
+      decryption(
+        await zkToken.balanceOf(clientB.address),
+        17942993n,
+        8967090n,
+        15889415n
+      )
+    ).to.eq(transferInputA.value)
+
+    console.log(
+      'Client A balance after transfer A to B',
+      await zkToken.balanceOf(clientA.address)
+    )
+
+    console.log(
+      'Client B balance after transfer A to B',
+      await zkToken.balanceOf(clientB.address)
+    )
+  })
+
+  it('Transfer B to A', async function () {
+    const tx = await zkToken.connect(clientB).transfer(
+      clientA.address,
+      [transferProofB.pi_a[0], transferProofB.pi_a[1]],
+      [
+        [transferProofB.pi_b[0][1], transferProofB.pi_b[0][0]],
+        [transferProofB.pi_b[1][1], transferProofB.pi_b[1][0]],
+      ],
+      [transferProofB.pi_c[0], transferProofB.pi_c[1]],
+      transferPublicB
+    )
+
+    const receipt = await tx.wait()
+
+    console.log(
+      'Gas used by transfer: ',
+      '\x1b[33m',
+      receipt.gasUsed.toString(),
+      '\x1b[0m'
+    )
+
+    expect(await zkToken.balanceOf(clientB.address)).to.eq(
+      transferInputB.newEncryptedBalance
+    )
+
+    expect(
+      decryption(
+        await zkToken.balanceOf(clientA.address),
+        46783589n,
+        11692464n,
+        39229921n
+      )
+    ).to.eq(BigInt(transferInputB.value) + 5n) // 5 - old balance
+
+    console.log(
+      'Client A balance after transfer B to A',
+      await zkToken.balanceOf(clientA.address)
+    )
+
+    console.log(
+      'Client B balance after transfer B to A',
+      await zkToken.balanceOf(clientB.address)
+    )
+  })
+
+  it('revert error registration', async function () {
+    await expect(
+      zkToken.connect(clientA).registration(
+        [transferProofA.pi_a[0], transferProofA.pi_a[1]],
+        [
+          [transferProofA.pi_b[0][1], transferProofA.pi_b[0][0]],
+          [transferProofA.pi_b[1][1], transferProofA.pi_b[1][0]],
+        ],
+        [transferProofA.pi_c[0], transferProofA.pi_c[1]],
+        registrationPublicA
+      )
+    ).to.be.revertedWith('you are registered')
+  })
+
+  it('onlyRegistered modifier', async function () {
+    await expect(
+      zkToken.connect(clientB).transfer(
+        clientC.address,
+        [transferProofB.pi_a[0], transferProofB.pi_a[1]],
+        [
+          [transferProofB.pi_b[0][1], transferProofB.pi_b[0][0]],
+          [transferProofB.pi_b[1][1], transferProofB.pi_b[1][0]],
+        ],
+        [transferProofB.pi_c[0], transferProofB.pi_c[1]],
+        transferPublicB
+      )
+    ).to.be.revertedWith('user not registered')
+  })
+})
+
+// exponentiation modulo
+function pow(base, exp, mod) {
+  let res = 1n
+  while (exp != 0n) {
+    if ((exp & 1n) != 0n) {
+      res = BigInt(res * base) % mod
+    }
+    base = BigInt(base * base) % mod
+    exp >>= 1n
+  }
+  return res
+}
+
+// Paye cryptosystem
+function div(val, by) {
+  return BigInt((val - (val % by)) / by)
+}
+
+function L(u, n) {
+  return div(BigInt(u) - 1n, BigInt(n))
+}
+
+function encryption(g, m, r, n) {
+  return BigInt(BigInt(pow(g, m, n * n) * pow(r, n, n * n)) % BigInt(n * n))
+}
+
+function decryption(c, n, l, mu) {
+  return BigInt(
+    BigInt(
+      L(pow(BigInt(c), BigInt(l), BigInt(n * n)), BigInt(n)) * BigInt(mu)
+    ) % BigInt(n)
+  )
+}
